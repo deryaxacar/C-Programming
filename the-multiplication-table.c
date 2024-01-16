7fc2fbcd88427fb204a9defd79e79a6d626e4dfd
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2024-01-16 22:56:03 +0300

    Update the-multiplication-table.c

--- the-multiplication-table.c
+++ the-multiplication-table.c
@@ -3,11 +3,11 @@
 int main()
 {
 
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 10; i++) 
+    {
         printf("\n");
-        for (int j = 1; j <= 10; j++) {
+        for (int j = 1; j <= 10; j++)
             printf("%d x %d = %d\n", i, j, i * j);
-        }
     }
     return 0;
 }
