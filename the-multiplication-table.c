5f01c9dde9da134b925ff042bff53dd47d589d22
Derya Acar <118841541+deryaxacar@users.noreply.github.com>
2024-01-16 22:59:14 +0300

    Update the-multiplication-table.c

--- the-multiplication-table.c
+++ the-multiplication-table.c
@@ -3,11 +3,11 @@
 int main()
 {
 
-    for (int i = 1; i <= 10; i++) 
+    for (int i = 1; i <= 10; i++) //Bu döngü, 1'den 10'a kadar olan sayıları temsil eden i' yi artırarak çalışır
     {
         printf("\n");
-        for (int j = 1; j <= 10; j++)
-            printf("%d x %d = %d\n", i, j, i * j);
+        for (int j = 1; j <= 10; j++) //Her bir i değeri için, 1'den 10'a kadar olan sayıları temsil eden j'yi artırarak çalışır.
+            printf("%d x %d = %d\n", i, j, i * j); //çarpımı ekrana yazdırılır.
     }
     return 0;
 }
